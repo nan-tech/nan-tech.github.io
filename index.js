8963f9080bbe7e1a60689d0db1ed19c89f959a96
whatsupdawg <netemadi@ucdavis.edu>
2019-05-09 21:40:16 -0700

    Updated index.html and index.js

--- index.js
+++ index.js
@@ -61,10 +61,10 @@ function updateTable() {
             let resourceID = $(this).attr('data-resourceID');
             DLib.Resources.getResourceByID(resourceID).then((resource) => {
                 $(".modal-title").text(`More Information: ${resource.name}`);
-                $(".modal-body").text(`Location: ${resource.location.address}`);
+                $(".modal-body").text(`Location: ${resource.location}`);
                 resource.details().then((details) => {
                     $(".modal-body").text(`Description: ${details.info}`);
-                  //  $(".modal-body").text(`Phone: ${details.phone}`);
+                    $(".modal-body").text(`Phone: ${details.contact.phone}`);
                     $('#myModal').modal('show');
                     console.log(details);
                 });
